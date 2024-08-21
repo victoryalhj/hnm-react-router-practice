c4afd7f5b7a2ed68eace703b80b035a6d164c353
victoryalhj <victoryalhj@gmail.com>
2024-08-21 12:22:15 +0100

    update navbar

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import {Routes, Route} from "react-router-dom";
 import ProductDetail from './page/ProductDetail';
 import ProductAll from './page/ProductAll';
 import Login from './page/Login';
+import Navbar from "./component/Navbar";
 
 // 1.전체상품페이지, 로그인, 상품페이지
+// 1-1.네비게이션바
 // 2.전체상품을 볼수있음
 // 3.로그인 버튼을 누르면 로그인 페이지가 나온다
 // 4.상품디테일을 눌렀으나, 로그인이 안되있을경우 로그인페이지
@@ -16,6 +18,7 @@ import Login from './page/Login';
 function App() {
   return (
     <div>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<ProductAll />} />
         <Route path="/login" element={<Login />} />
---
+++ src/component/Navbar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faUser} from "@fortawesome/free-regular-svg-icons";
+import {faSearch} from "fortawesome/free-solid-svg-icons"
+
+
+const Navbar = () => {
+  const menuList = ["여성","Divided","남성","신생아/유아","아동","h&m home","sale","지속가능성"]
+  return (
+    <div>
+      <div>
+        <div className='login-button'>
+          <FontAwesomeIcon icon={faUser} />
+          <div>로그인</div> 
+        </div>
+      </div>
+      <div className="nav-section">
+        <img 
+        width={100} 
+        src="https://www.hm.com/entrance/assets/bundle/img/HM-Share-Image.jpg" />
+      </div>
+      <div className="menu-area">
+          <ul className="menu-list">
+            {menuList.map((menu) => (
+              <li>{menu}</li>
+            ))}
+          </ul>
+      
+        <div>
+          <FontAwesomeIcon icon={faSearch}/>
+          <input type="text" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
