c4f3eb8751973e44c3c7f892a523b9d6060cb7f5
victoryalhj <victoryalhj@gmail.com>
2024-08-21 12:52:44 +0100

    update navbar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faUser} from "@fortawesome/free-regular-svg-icons";
-import {faSearch} from "fortawesome/free-solid-svg-icons"
+import {faSearch} from "@fortawesome/free-solid-svg-icons"
 
 
 const Navbar = () => {
