4d959ab3722b64a3e9798b0408497e713e1e3d2b
victoryalhj <victoryalhj@gmail.com>
2024-08-22 11:53:38 +0100

    Error fixed

--- src/route/PrivateRoute.js
+++ src/route/PrivateRoute.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ProductDetail from '../page/ProductDetail';
 import { Navigate } from "react-router-dom";
+import ProductDetail from '../page/ProductDetail';
 
 const PrivateRoute = ({authenticate}) => {
   return authenticate === true ? <ProductDetail /> : <Navigate to="/login" />;
