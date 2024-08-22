c148b4c129a8536a846a0380737dc70c793a1e52
victoryalhj <victoryalhj@gmail.com>
2024-08-22 11:42:15 +0100

    useCallback

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -1,18 +1,18 @@
-import React,{useCallback, useEffect,useState} from "react";
+import React,{useCallback,useEffect,useState} from "react";
 import {Container,Row,Col} from "react-bootstrap"
 import { useParams } from "react-router-dom";
 
 const ProductDetail = () => {
-  let{id} = useParams()
+  let{id} = useParams();
   const [product,setProduct] = useState(null);
   
-  const getProductDetail = async()=>{
+  const getProductDetail = useCallback(async()=>{
     let url = `http://localhost:7000/products/${id}`
     let response = await fetch(url);
     let data = await response.json();
     console.log(data);
     setProduct(data);
-  }
+  },[id]);
 
   useEffect(()=> {
     getProductDetail();
@@ -21,7 +21,7 @@ const ProductDetail = () => {
   return (<Container>
         <Row>
           <Col className="product-img">
-          <img src={product?.img}/>
+          <img src={product?.img} alt=""/>
           </Col>
           <Col>
             <div>{product?.title}</div>
