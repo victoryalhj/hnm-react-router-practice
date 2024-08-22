e08512f2b9188d358b8ed127dd97de6b30409d0f
victoryalhj <victoryalhj@gmail.com>
2024-08-22 11:07:50 +0100

    product detail update

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React,{useEffect,useState} from "react";
+import {Container,Row,Col} from "react-bootstrap"
+import { useParams } from "react-router-dom";
 
 const ProductDetail = () => {
-  return (
-    <div>상품상세 페이지</div>
+  let{id} = useParams()
+  const [product,setProduct] = useState(null);
+  const getProductDetail = async()=>{
+    let url = `http://localhost:7000/products/${id}`
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+    setProduct(data);
+  }
+  useEffect(()=> {
+    getProductDetail()
+  },[])
+
+  return (<Container>
+        <Row>
+          <Col className="product-img">
+          <img src={product?.img}/>
+          </Col>
+          <Col>
+            <div>{product?.title}</div>
+            <div>{product?.price}</div>
+          </Col>
+        </Row>
+      </Container>
   );
 };
 
