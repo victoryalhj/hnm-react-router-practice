b8ffe807cce3a8238cc8d911f4bad7a0dfcf307f
victoryalhj <victoryalhj@gmail.com>
2024-08-22 12:01:34 +0100

    Error fixed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {useEffect, useState} from "react";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Routes, Route} from "react-router-dom";
-import ProductDetail from './page/ProductDetail';
+// import ProductDetail from './page/ProductDetail';
 import ProductAll from './page/ProductAll';
 import Login from './page/Login';
 import Navbar from "./component/Navbar";
--- src/route/PrivateRoute.js
+++ src/route/PrivateRoute.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import ProductDetail from '../page/ProductDetail';
 import { Navigate } from "react-router-dom";
-import ProductDetail from '../page/ProductDetail';
 
-const PrivateRoute = ({authenticate}) => {
-  return authenticate === true ? <ProductDetail /> : <Navigate to="/login" />;
+const PrivateRoute = ({ authenticate }) => {
+  return authenticate ? <ProductDetail /> : <Navigate to="/login" />;
 };
 
 export default PrivateRoute
