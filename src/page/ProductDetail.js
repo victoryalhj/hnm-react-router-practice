d491afbcf5314e46ba6dee8df103abf6113ecad2
victoryalhj <victoryalhj@gmail.com>
2024-08-22 11:20:42 +0100

    product detail update

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -1,10 +1,11 @@
-import React,{useEffect,useState} from "react";
+import React,{useCallback, useEffect,useState} from "react";
 import {Container,Row,Col} from "react-bootstrap"
 import { useParams } from "react-router-dom";
 
 const ProductDetail = () => {
   let{id} = useParams()
   const [product,setProduct] = useState(null);
+  
   const getProductDetail = async()=>{
     let url = `http://localhost:7000/products/${id}`
     let response = await fetch(url);
@@ -12,9 +13,10 @@ const ProductDetail = () => {
     console.log(data);
     setProduct(data);
   }
+
   useEffect(()=> {
-    getProductDetail()
-  },[])
+    getProductDetail();
+  },[getProductDetail]);
 
   return (<Container>
         <Row>
